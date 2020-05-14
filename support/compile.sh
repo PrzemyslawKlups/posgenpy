202a39eaed9d6982030291d0167e54b183cd35d0
D Haley <daniel@materials>
2020-05-14 20:36:21 +0100

    * Add posgen to compilation

--- support/compile.sh
+++ support/compile.sh
@@ -5,12 +5,19 @@ if [ -d libatomprobe ] ; then
 	exit 1;
 fi
 
+if [ -d posgen ] ; then 
+	echo "posgen dir already exists. Refusing to continue";
+	exit 1;
+fi
+
 #Install build dependencies
 sudo apt-get install build-essential cmake mercurial python3-dev swig || { echo "Failed to download libraries. Aborting."; exit 1 ; }
 
 #Install actual depdendencies
-sudo apt-get install libxml2-dev libgsl-dev libqhull-dev
+sudo apt-get install libxml2-dev libgsl-dev libqhull-dev libmuparser-dev
 
+#Download and compile libatomprobe
+#=====
 CLONEURL=https://hg.sr.ht/~mycae/libatomprobe
 hg clone $CLONEURL || { echo "Failed to obtain libraries. Aborting."; exit 1 ; }
 cd libatomprobe/
@@ -25,3 +32,22 @@ make -j2  || { echo "library compilation failed. Aborting"; exit 1 ; }
 sudo make install
 #Run linker configurer
 sudo ldconfig
+
+cd ..
+#=========
+
+#Download and compile posgen
+#==========
+POSGEN_CLONE=http://hg.code.sf.net/p/apttools/posgen/code 
+hg clone $POSGEN_CLONE posgen || { echo "Failed to download posgen source code. Aborting."; exit 1 ; }
+
+cd posgen
+
+make || { echo "Failed to build posgen. Aborting" ; exit 1 ; } 
+sudo make install || {echo "Failed to install posgen. Aborting" ; exit 1 ; }
+
+cd ..
+#=========
+
+
+echo " Completed installation."
